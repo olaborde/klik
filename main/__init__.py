4290076ffec7bae5edb93e2f4f3755e006ed3626
Osse Laborde <oml007@live.com>
2020-11-18 13:32:55 -0500

    fix errors with table relationship

--- main/__init__.py
+++ main/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 app.config["DEBUG"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/klik'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
